17124c96995e741a7c6ce933b4a59912721a6958
Elias <echavela@nd.edu>
2026-09-23 12:25:11 -0400

    Init

---
+++ AFH.py
@@ -0,0 +1,15 @@
+
+# https://www.bluetooth.com/bluetooth-le-primer/
+# https://docs.silabs.com/bluetooth/latest/bluetooth-fundamentals-system-performance/afh
+# https://www.bluetooth.com/wp-content/uploads/Files/Specification/HTML/Core-54/out/en/low-energy-controller/link-layer-specification.html
+# https://github.com/zephyrproject-rtos/zephyr/blob/main/subsys/bluetooth/controller/ll_sw/lll_chan.c
+# https://plotly.com/python/interactive-html-export/
+
+import csv
+from pathlib import Path
+
+import plotly.graph_objects as go
+
+
+
+runner()
